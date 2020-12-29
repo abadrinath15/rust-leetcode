ee783ecefddf7965d96fb59dafb307de8d81a1dc
Aditya Badrinath <abadrinath15@gmail.com>
2020-12-29 10:35:39 -0500

    think this is most rust like

--- src/solution_files/problem_14.rs
+++ src/solution_files/problem_14.rs
@@ -2,32 +2,30 @@ use crate::solution_files::Solution;
 
 impl Solution {
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
-        let mut strs_c = strs.clone();
         let mut shortest: String;
-        let mut shortest_t;
-        match strs_c.pop() {
-            None => shortest = String::new(),
-            Some(x) => shortest = x,
-        };
-        for word in strs_c {
-            if shortest.len() == 0 {
-                break;
-            }
-            else if word.len() < shortest.len() {
-                shortest = shortest[0..word.len()].to_string();
-            }
-            shortest_t = String::new();
-            for x_y in shortest.chars().zip(word.chars()) {
-                let (x, y) = x_y;
-                if x == y {
-                    shortest_t.push(x);
-                }
-                else {
+        let mut shortest_t: String;
+        if let Some(first) = strs.first() {
+            shortest = first.to_string();
+            for word in &strs[1..] {
+                if shortest.len() == 0 {
                     break;
+                } else if word.len() < shortest.len() {
+                    shortest = shortest[0..word.len()].to_string();
                 }
-           }
-           shortest = shortest_t;
-        };
-        shortest
+                shortest_t = String::new();
+                for x_y in shortest.chars().zip(word.chars()) {
+                    let (x, y) = x_y;
+                    if x == y {
+                        shortest_t.push(x);
+                    } else {
+                        break;
+                    }
+                }
+                shortest = shortest_t;
+            }
+            shortest
+        } else {
+            String::from("")
+        }
     }
 }
